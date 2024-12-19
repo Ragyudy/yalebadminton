e918d9aec009128dfad00b5f8fa5f25afaa45de5
ragyudy <gradyzyu@gmail.com>
2024-12-19 15:40:59 -0500

    init new react website

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Team from "./pages/Team";
-import Gallery from "./pages/Gallery";
-import Faq from "./pages/Faq";
-import GlobalStyle from "./GlobalStyle";
-import ComingSoon from "./pages/ComingSoon";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Team" element={<Team />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/Faq" element={<Faq />} />
-        <Route path="/coming-soon" element={<ComingSoon />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
