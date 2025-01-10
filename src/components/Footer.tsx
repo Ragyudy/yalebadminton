bffb039c81bdee8daca2c36b3851331e1d8d8e22
ragyudy <gradyzyu@gmail.com>
2025-01-10 11:40:46 -0500

    footer small changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import { Instagram, Facebook, Twitter, Youtube } from "react-bootstrap-icons";
+import { Instagram, Facebook, TwitterX, Youtube } from "react-bootstrap-icons";
 
 export function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-800 py-6 px-4 border-t border-gray-300">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center py-5">
         {/* Logo and Copyright */}
         <div className="flex items-center gap-2">
           <img
@@ -11,13 +11,13 @@ export function Footer() {
             alt="Yale Badminton Club"
             className="h-8"
           />
-          <span className="text-sm">
-            &copy; 2025 Yale Club Badminton. All rights reserved.
+          <span className="text-sm text-gray-500">
+            &copy; 2025 Yale Club Badminton – All rights reserved.
           </span>
         </div>
 
         {/* Social Media Links */}
-        <div className="flex gap-4 mt-4 md:mt-0">
+        <div className="flex gap-6 mt-4 md:mt-0">
           <a
             href="https://www.instagram.com/yalebadminton/"
             target="_blank"
@@ -33,18 +33,18 @@ export function Footer() {
             <Facebook size={24} />
           </a>
           <a
+            href="https://twitter.com/yalebc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TwitterX size={24} />
+          </a>
+          <a
             href="https://www.youtube.com/user/yalebadmintonclub"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Youtube size={24} />
-          </a>
-          <a
-            href="https://twitter.com/yalebc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Twitter size={24} />
           </a>
         </div>
       </div>
