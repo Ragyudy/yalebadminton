ad40348b9bec689fa4ed79a89bbfaef9b6292a99
ragyudy <gradyzyu@gmail.com>
2025-02-26 18:11:51 -0500

    add disclaimer to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,15 @@ export function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-800 py-2 border-t border-gray-300">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center py-5">
-        {/* Logo and Copyright */}
         <div className="flex items-center gap-2">
           <img
             src="/favicon-light.svg"
             alt="Yale Badminton Club"
             className="h-8"
           />
-          <span className="text-sm text-gray-500">
-            &copy; 2025 Yale Club Badminton – All rights reserved.
-          </span>
+          <span className="text-lg text-blue-900">Yale Club Badminton</span>
         </div>
 
-        {/* Social Media Links */}
         <div className="flex gap-6 mt-4 md:mt-0">
           <a
             href="https://www.instagram.com/yalebadminton/"
@@ -47,6 +43,14 @@ export function Footer() {
             <Youtube size={24} />
           </a>
         </div>
+      </div>
+      <div className="text-sm text-gray-600 px-4 pb-4">
+        “Yale” and “Yale University” are registered trademarks of Yale
+        University. This website is a student-run website and is maintained,
+        hosted, and operated independently of Yale University. The activities on
+        this website are not supervised or endorsed by Yale, and information
+        contained on this website does not necessarily reflect the opinions or
+        official positions of the University.
       </div>
     </footer>
   );
